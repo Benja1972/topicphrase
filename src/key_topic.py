8c36280b0d8a29df6ae6838096bbe33d5d3ed341
Sergei Rybalko <sergei@alpha10x.com>
2023-10-12 14:57:48 +0200

    tested new class KeyPhraser

--- src/key_topic.py
+++ src/key_topic.py
@@ -12,6 +12,52 @@ from pprint import pprint
 import umap
 import hdbscan
 
+
+
+@Language.component('merge_compound')
+def merge_compounds(d):
+    """ Merge compounds to be one token
+
+    A compound is two tokens separated by a hyphen when the tokens are right next to the hyphen
+
+    d (spacy.Doc): Document
+    Returns: spacy.Doc
+
+    > [t.text for t in nlp('peer-to-peer-to-peer')]
+    ['peer', '-', 'to', '-', 'peer', 'to', '-', 'peer']
+    > [t.text for t in merge_compounds(nlp('peer-to-peer-to-peer'))]
+    ['peer-to-peer-to-peer']
+    """
+    # Returns beginning and end offset of spacy.Token
+    offsets = lambda t: (t.idx, t.idx+len(t))
+
+    # Identify the hyphens
+    # for each token is it a hyphen and the next and preceding token are right next to the hyphen
+    spans = [(i-1, i+1) for i in range(len(d))
+             if i != 0 and i != len(d) and d[i].text == '-' and \
+                offsets(d[i-1])[1] == offsets(d[i])[0] and \
+                offsets(d[i+1])[0] == offsets(d[i])[1]
+            ]
+    # merging spans to account for multi-compound terms
+    merged = []
+    for i, (b, e) in enumerate(spans):
+        # if the last spans ends when the current span begins,
+        # merge those
+        if merged and b == merged[-1][1]:
+            merged[-1] = (merged[-1][0], e)
+        else:
+            merged.append((b, e))
+
+    # Merge the compounds in the document
+    with d.retokenize() as retok:
+        for b, e in merged:
+            retok.merge(d[b:e+1], attrs={
+                'POS': d[b].pos_,
+                'LEMMA': ''.join(t.lemma_ for t in d[b:e+1])
+            })
+    return d
+
+
 # Package class
 class KeyPhraser:
     def __init__(self, 
@@ -83,7 +129,7 @@ class KeyPhraser:
         self.labels  = cls.labels_
 
 
-    def sorted_keywords(self, sort_by="centroid"):
+    def sorted_topics(self, sort_by="centroid"):
         wn = []
         for lc in set(self.labels):
             si = list(np.where(self.labels==lc)[0])
@@ -178,48 +224,6 @@ def get_clusters(word_emb, min_cluster_size=10):
     return lbs
 
 
-@Language.component('merge_compound')
-def merge_compounds(d):
-    """ Merge compounds to be one token
-
-    A compound is two tokens separated by a hyphen when the tokens are right next to the hyphen
-
-    d (spacy.Doc): Document
-    Returns: spacy.Doc
-
-    > [t.text for t in nlp('peer-to-peer-to-peer')]
-    ['peer', '-', 'to', '-', 'peer', 'to', '-', 'peer']
-    > [t.text for t in merge_compounds(nlp('peer-to-peer-to-peer'))]
-    ['peer-to-peer-to-peer']
-    """
-    # Returns beginning and end offset of spacy.Token
-    offsets = lambda t: (t.idx, t.idx+len(t))
-
-    # Identify the hyphens
-    # for each token is it a hyphen and the next and preceding token are right next to the hyphen
-    spans = [(i-1, i+1) for i in range(len(d))
-             if i != 0 and i != len(d) and d[i].text == '-' and \
-                offsets(d[i-1])[1] == offsets(d[i])[0] and \
-                offsets(d[i+1])[0] == offsets(d[i])[1]
-            ]
-    # merging spans to account for multi-compound terms
-    merged = []
-    for i, (b, e) in enumerate(spans):
-        # if the last spans ends when the current span begins,
-        # merge those
-        if merged and b == merged[-1][1]:
-            merged[-1] = (merged[-1][0], e)
-        else:
-            merged.append((b, e))
-
-    # Merge the compounds in the document
-    with d.retokenize() as retok:
-        for b, e in merged:
-            retok.merge(d[b:e+1], attrs={
-                'POS': d[b].pos_,
-                'LEMMA': ''.join(t.lemma_ for t in d[b:e+1])
-            })
-    return d
 
 def centr_sort(words, word_emb, lbs, doc_emb, sort_by="centroid"):
     wn = []
@@ -250,11 +254,7 @@ def centr_sort(words, word_emb, lbs, doc_emb, sort_by="centroid"):
 
 if __name__ == '__main__':
 
-    # Load data  and embedder ============================
-
-    nm = 'Self-driving_car'
-    print(nm)
-    print('='*20)
+    # = Load data  ===============================
 
     f_in  = '../data/self-car.txt'
     docs = []
@@ -264,32 +264,19 @@ if __name__ == '__main__':
             docs.append(dcc.strip('\r\n'))
 
     doc = ' '.join(docs[:5])
-
-    model = 'distilbert-base-nli-stsb-mean-tokens'
-    sbert = SentenceTransformer(model)
-    # ===========================================
-
-
-    # Get candidates =============
-  
-    # Add custom stoplist
-    # ~ stoplist = []
-
-    # ~ words = get_candidates(doc)
-    words = get_grammar_candidates(doc)
-
-    #  Embded ================
-    word_emb = sbert.encode(words)
-    doc_emb = sbert.encode([doc])
-
-    # Cluster ======================
-    lbs = get_clusters(word_emb,6)
-
-    # Group and sort 
-    wsr_c = centr_sort(words, word_emb, lbs, doc_emb)
-    wsr_d = centr_sort(words, word_emb, lbs, doc_emb, sort_by="doc")
+    
+    # = Initiate key-phrases extractor ===========
+    kph = KeyPhraser()
+    
+    # = Fit document ============================
+    kph.fit(doc)
 
 
+    # = Get sorted topics  ======================
+    wsr_d = kph.sorted_topics(sort_by="doc")
+    wsr_c = kph.sorted_topics(sort_by="centroid")
+    
+    # = Print topics  ==========================
     print("\n\nSorted by centroids\n")
     for w in wsr_c:
         print(w[0], w[1][:3])
